abda92e9ded0e167d7c4b7ed2d757805b7eb0ab1
Welleson <wellesonlukas@gmail.com>
2021-04-15 16:04:41 -0300

    view e template edição de empresa

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('empresas/', listar_empresas, name='listar_empresas'),
     path('create/empresa', cadastrar_empresa, name='cadastrar_empresa'),
     path('detail/empresa/<int:pk>', detalhes_empresa, name='detalhes_empresa'),
+    path('edit/empresa/<int:pk>', editar_empresa, name='editar_empresa'),
 ]
--- core/views/empresas_views.py
+++ core/views/empresas_views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+
+
 from core.models import Empresa
 from core.forms.empresas_forms import EmpresasForm
 from usuario.models import Usuario
+from api.models import Produto
 from django.contrib.auth.models import User
 
 
@@ -43,9 +46,40 @@ def cadastrar_empresa(request, template_name='administracao/empresas/empresas_fo
         return redirect('index')
 
 
+def editar_empresa(request, pk, template_name='administracao/empresas/empresas_form.html'):
+    if request.user.is_superuser:
+        empresa = get_object_or_404(Empresa, pk=pk)
+        if request.method == 'POST':
+            form = EmpresasForm(request.POST, instance=empresa)
+            if form.is_valid():
+                empresa = form.save(commit=False)
+                empresa.save()
+                messages.success(request, "Empresa atualizada com sucesso!")
+                return redirect('listar_empresas')
+            else:
+                messages.error(request, "Ops, não foi possivel cadastrar a empresa")
+        else:
+            form = EmpresasForm(instance=empresa)
+        return render(request, template_name, {'form': form})
+    else:
+        messages.error(request, "Ops, o usuário não tem permissão!")
+        return redirect('index')
+
+
+
+
 def detalhes_empresa(request, pk, template_name='administracao/empresas/empresas_detalhes.html'):
     empresa = get_object_or_404(Empresa, pk=pk)
     usuarios = Usuario.objects.filter(empresa=empresa.pk)
 
-    print(usuarios)
     return render(request, template_name, {'empresa': empresa, 'usuarios': usuarios})
+
+
+
+
+#PEGAR EMPRESA E PRODUTOS DO USUARIO LOGADO
+#usuario_id = request.user.id
+#empresa = Empresa.objects.filter(empresa__usuario_id=usuario_id)
+#print(empresa)
+#produtos = list(Produto.objects.filter(empresa_id=1).values())
+#print(produtos)
