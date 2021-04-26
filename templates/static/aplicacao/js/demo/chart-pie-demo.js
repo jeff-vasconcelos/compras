d2bf29af9a7d820e2090c3959c105011634971b5
Welleson <wellesonlukas@gmail.com>
2021-04-26 13:36:52 -0300

    template home finalizado

--- templates/static/aplicacao/js/demo/chart-pie-demo.js
+++ templates/static/aplicacao/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Curva A", "Curva B", "Curva C", "Curva C", "Curva D", "Curva E", "Curva X"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [40, 25, 15, 10, 6.5, 3.5],
+      backgroundColor: ['#02e591', '#4791ff', '#54d8ff', '#a3a0fb', '#ffd950', '#ec6666'],
+      hoverBackgroundColor: ['#02e591', '#4791ff', '#54d8ff', '#a3a0fb', '#ffd950', '#ec6666'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
