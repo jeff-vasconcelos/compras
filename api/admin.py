89527fff287d92b2bdeeb16bd4f6f57563a23f48
welleson-lukas <wellesonlukas@gmail.com>
2021-05-14 18:01:44 -0300

    pós migracoes

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from api.models.produto_models import Produto
+from api.models.avarias_models import Avaria
+from api.models.estoque_atual_models import EstoqueAtual
 from api.models.fornecedor_models import Fornecedor
+from api.models.hist_estoque_models import HistEstoque
+from api.models.p_compras_models import PedidoCompras
+from api.models.produto_models import Produto
+from api.models.ultima_entrada_models import UltimaEntrada
+from api.models.vendas_models import Venda
 
 """ Adicionando dados da API de Produtos no Admin do DJANGO """
 
@@ -13,4 +19,10 @@ class Produtos(admin.ModelAdmin):
 
 
 admin.site.register(Produto, Produtos)
+admin.site.register(Avaria)
+admin.site.register(EstoqueAtual)
 admin.site.register(Fornecedor)
+admin.site.register(HistEstoque)
+admin.site.register(PedidoCompras)
+admin.site.register(UltimaEntrada)
+admin.site.register(Venda)
