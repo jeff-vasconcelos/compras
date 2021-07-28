a16d6f31b6aff26cb875998bfa963471341cdf81
Welleson Lukas <56523073+welleson-lukas@users.noreply.github.com>
2021-07-28 19:08:53 -0300

    Update produto.py

--- api/models/produto.py
+++ api/models/produto.py
@@ -4,12 +4,12 @@ from api.models.fornecedor import Fornecedor
 
 
 class Produto(models.Model):
-    cod_produto = models.IntegerField(null=True, blank=True)
-    desc_produto = models.CharField(max_length=255, null=True, blank=True)
+    cod_produto = models.IntegerField(null=False, blank=False)
+    desc_produto = models.CharField(max_length=255, null=False, blank=False)
     embalagem = models.CharField(max_length=255, null=True, blank=True)
-    quantidade_un_cx = models.FloatField(blank=True, null=True)
-    marca = models.CharField(max_length=255, blank=True, null=True)
-    peso_liquido = models.CharField(max_length=255, blank=True, null=True)
+    quantidade_un_cx = models.FloatField(null=False, blank=False)
+    marca = models.CharField(max_length=255, null=False, blank=False)
+    peso_liquido = models.CharField(max_length=255, null=False, blank=False)
     principio_ativo = models.CharField(max_length=255, null=True, blank=True)
     cod_fabrica = models.IntegerField(null=True, blank=True)
     cod_ncm = models.CharField(max_length=255, null=True, blank=True)
@@ -18,12 +18,17 @@ class Produto(models.Model):
     cod_sec = models.IntegerField(null=True, blank=True)
     desc_departamento = models.CharField(max_length=255, null=True, blank=True)
     desc_secao = models.CharField(max_length=255, null=True, blank=True)
-
-    cod_fornecedor = models.IntegerField(null=True, blank=True)
+    cod_fornecedor = models.IntegerField(null=False, blank=False)
+    
     fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE, related_name='fornecedor_produto',
                                 blank=True, null=True)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='produto',
                                 blank=True, null=True)
+    
+    campo_um = models.CharField(max_lenght=255, null=True, blank=True)
+    campo_dois = models.CharField(max_lenght=255, null=True, blank=True)
+    campo_tres = models.CharField(max_lenght=255, null=True, blank=True)
+
 
     def save(self, *args, **kwargs):
         if not self.fornecedor:
