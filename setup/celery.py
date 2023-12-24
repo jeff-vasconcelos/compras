b37baf2ae12380f6bb9df92276dd6a31624fec78
root <root@srv437397.hstgr.cloud>
2023-12-24 16:37:00 +0000

    celery default schedule removed

--- setup/celery.py
+++ setup/celery.py
@@ -6,30 +6,11 @@ from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
-app = Celery('setup', broker="redis://localhost:6379", backend="redis://localhost:6379")
+app = Celery('setup', broker="redis://redis:6379", backend="redis://redis:6379")
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.beat_schedule = {
-
-    # ALERTA
-    'alerta': {
-        'task': 'core.tasks.processa_alerta',
-        'schedule': 1,
-        "args": ("request", 0)
-    },
-
-    #EMAIL
-    'email': {
-        'task': 'core.tasks.processa_email',
-        'schedule': 1,
-        "args": ("request", 0)
-    },
-
-
-
-}
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
